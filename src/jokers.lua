de401b77491801a813c3f53b57cbca192f0e84e8
Thomas-Ruby <thomasruby@ufl.edu>
2025-12-21 19:00:28 -0500

    fix joker2

--- src/jokers.lua
+++ src/jokers.lua
@@ -3,18 +3,20 @@ SMODS.Joker {
     key = "joker2",
     loc_txt = {
         name = "Joker 2",
-        text = "This is the in-game description for this joker. {C:mult}+#1# {} Mult" --#1# is a variable that's stored in config, and is put into loc_vars. C:mult is coloring.
-    }
+        text = {
+           "This is the in-game description of this joker. {C:mult}+#1# {} Mult" -- #1# is a variable that's stored in config, and is put into loc_vars. C:mult is coloring.
+        }
+    },
     atlas = "Bamlatro",
-    pos = { x = 0, y = 0 },
+    pos = { x = 2, y = 1 },
     rarity = 1,
     blueprint_compat = true,
     cost = 2,
     discovered = true,
     config = { extra = { mult = 4 }, },
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult } }
-    end,
+		return { vars = { card.ability.extra.mult } }
+	end,
     calculate = function(self, card, context)
         if context.joker_main then
             return {
