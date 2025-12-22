fe817e4338dd40e6c80ba2e42b16fcabde121466
Thomas-Ruby <thomasruby@ufl.edu>
2025-12-22 13:46:22 -0500

    Add initial feedthebeast

--- jokers/jokers.lua
+++ jokers/jokers.lua
@@ -1,27 +1,61 @@
--- Joker
-SMODS.Joker {
-    key = "joker2",
+SMODS.Joker{
+    key = "feedthebeast",
     loc_txt = {
-        name = "Joker 2",
+        name = "Feed The Beast",
         text = {
-           "This is the in-game description of this joker. {C:mult}+#1# {} Mult" -- #1# is a variable that's stored in config, and is put into loc_vars. C:mult is coloring.
+            "{C:attention}Every hand{}, the rightmost",
+            "played card is {C:attention}destroyed{}, and",
+            "this joker gains {X:mult,C:white} X#1# {} Mult.",
+            "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)"
         }
     },
-    atlas = "Bamlatro",
-    pos = { x = 0, y = 0 },
-    rarity = 1,
-    blueprint_compat = true,
-    cost = 2,
-    discovered = true,
-    config = { extra = { mult = 4 }, },
+    config = {
+        extra = {
+            Xmult = 1,
+            increment = 0.1,
+            base = 1
+        }
+    },
     loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.mult } }
+		return { vars = {card.ability.extra.increment, card.ability.extra.Xmult} }
 	end,
+    atlas = 'Bamlatro',
+    pos = { x = 1, y = 0 },
+    rarity = 3,
+    cost = 9,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+
     calculate = function(self, card, context)
-        if context.joker_main then
+
+		if context.joker_main then
             return {
-                mult = card.ability.extra.mult
+                Xmult_mod = card.ability.extra.Xmult,
+                message = localize { type = "variable", key = "a_xmult", vars = { card.ability.extra.Xmult }}
             }
+		end
+
+        if context.after then
+            local c = context.full_hand[#context.full_hand]
+            G.E_MANAGER:add_event(Event({
+                blocking = true,
+                --delay = 4,
+                --after = true,
+                func = function()
+                    c:juice_up(0.8, 0.8)
+                    c:start_dissolve({ HEX("049100") }, nil, 1)
+                    return true
+                end
+            }))
+            c.destroyed = true
+            card.ability.extra.Xmult = card.ability.extra.Xmult + (card.ability.extra.increment * card.ability.extra.base)
+            return {
+				message = 'Eaten!',
+				colour = G.C.GREEN,
+				card = card
+			}
         end
-    end
+	end
 }
